c495a84ad15021284e00f6f40514f6f025ef4694
Gabriel Riba <griba2001@gmail.com>
2014-11-05 17:30:20 +0100

    listOfInts posNegOrder

--- src/TrieAble/Instances/IListOfInts.hs
+++ src/TrieAble/Instances/IListOfInts.hs
@@ -1,4 +1,10 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-|
+ListOfInts / byteString conversions to use in prefix trees (Tries)
+
+Since negative Integers have binary representations higher as Word than positive ones
+I add a posNegOrder prefix to the Big endian word8 rep. to restore negatives position
+-}
 module TrieAble.Instances.IListOfInts(
   propListOfInt32TrieAble,
   propListOfInt16TrieAble,
@@ -6,7 +12,7 @@ module TrieAble.Instances.IListOfInts(
 
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
-import Data.ByteString.Builder (int32BE, int16BE, toLazyByteString)
+import Data.ByteString.Builder (Builder, int32BE, int16BE, word8, toLazyByteString)
 import Data.ByteString.Lazy as LBS (toStrict, fromStrict)
 import qualified Data.List as L
 import qualified Data.Foldable as F
@@ -14,6 +20,8 @@ import Data.Bits
 import Data.Word
 import Data.Int
 import Control.Exception (assert)
+import Control.Applicative (liftA2)
+import Data.Monoid
 
 import TrieAble.TrieAble
 
@@ -21,12 +29,12 @@ import TrieAble.TrieAble
 (.$) = flip ($)
 
 instance TrieAble [Int32] where
-  toByteString = toStrict . toLazyByteString . F.foldMap int32BE
-  fromByteString = L.map (fromIntegral . bs2Word32) . chunksOf 4 . BS.unpack
+  toByteString = toStrict . toLazyByteString . F.foldMap combine32
+  fromByteString = L.map (fromIntegral . bs2Word32 . drop 1) . chunksOf 5 . BS.unpack
 
 instance TrieAble [Int16] where
-  toByteString = toStrict . toLazyByteString . F.foldMap int16BE
-  fromByteString = L.map (fromIntegral . bs2Word16) . chunksOf 2 . BS.unpack
+  toByteString = toStrict . toLazyByteString . F.foldMap combine16
+  fromByteString = L.map (fromIntegral . bs2Word16 . drop 1) . chunksOf 3 . BS.unpack
 
 chunksOf :: Int -> [Word8] -> [[Word8]]  
 chunksOf n xs = assert (length xs `mod` n == 0) .  L.unfoldr (chunk n) $ xs
@@ -36,7 +44,16 @@ chunksOf n xs = assert (length xs `mod` n == 0) .  L.unfoldr (chunk n) $ xs
                  else Just (firsts, rest)
         where
           (firsts, rest) = splitAt n w8l
-  
+
+posNegOrder :: (Num a, Ord a, Integral a) => a -> Word8
+posNegOrder = fromIntegral . (\x -> if x >= 0 then 1 else 0)
+
+combine32 :: Int32 -> Builder
+combine32 = liftA2 (mappend) (word8 . posNegOrder) int32BE
+
+combine16 :: Int16 -> Builder
+combine16 = liftA2 (mappend) (word8 . posNegOrder) int16BE
+
 
 bs2Word32 :: [Word8] -> Word32
 bs2Word32 [t,h,m,l] = shiftLFrom t 24 .|. shiftLFrom h 16 .|. shiftLFrom m 8 .|. fromIntegral l
